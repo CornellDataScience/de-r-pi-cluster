9f41e11c11a2604a5bfc266bc7f7aee4117f2930
Haram Kim <kimharam@gmail.com>
2018-11-18 12:38:47 -0500

    added face recognition skeleton

--- client.py
+++ client.py
@@ -4,6 +4,10 @@ import socket
 from os import linesep as ls
 import json
 
+def recognize_faces(url):
+    # takes a URL for an image, downloads, finds faces, and returns a dict of results
+    return {}
+
 async def run(dest):
     async with websockets.connect(
             'ws://' + dest) as websocket:
@@ -20,11 +24,12 @@ async def run(dest):
             print(f"received problem: {problem}")
 
             # TODO: do processing here
-            solution = str(int(problem["content"]) + 1)
-            time.sleep(5)
+            # solution = str(int(problem["content"]) + 1)
+            # time.sleep(5)
+            # solution_package = {"content": solution}
+            solution_package = recognize_faces(problem["url"])
 
             # send result back to server
-            solution_package = {"content": solution}
             await websocket.send(json.dumps(solution_package))
             print(f"sent solution: {solution_package}")
             print(ls)
