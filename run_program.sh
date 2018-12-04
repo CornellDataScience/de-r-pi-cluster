357c20e2cbf009cb050c4456ee57f57a5ba30dae
Haram Kim <kimharam@gmail.com>
2018-12-04 15:14:15 -0500

    fix to run script

--- run_program.sh
+++ run_program.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+3.6venv
 python main.py server &
 
-ssh cds-pi-1 "3.6venv; cd ~/de-r-pi-cluster; python main.py localhost:8765 &"
-ssh cds-pi-2 "3.6venv; cd ~/de-r-pi-cluster; python main.py localhost:8765 &"
-ssh cds-pi-3 "3.6venv; cd ~/de-r-pi-cluster; python main.py localhost:8765 &"
+ssh cds-pi-1 "3.6venv; cd ~/de-r-pi-cluster; python main.py localhost:8765" &
+ssh cds-pi-2 "3.6venv; cd ~/de-r-pi-cluster; python main.py localhost:8765" &
+ssh cds-pi-3 "3.6venv; cd ~/de-r-pi-cluster; python main.py localhost:8765" &
 
 fg
