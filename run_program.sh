3d3754b0e28656d00bfafc1b1aa1c3f48c581ad0
Haram Kim <kimharam@gmail.com>
2018-12-04 15:09:39 -0500

    added start script

---
+++ run_program.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python main.py server &
+
+ssh cds-pi-1 "3.6venv; cd ~/de-r-pi-cluster; python main.py localhost:8765 &"
+ssh cds-pi-2 "3.6venv; cd ~/de-r-pi-cluster; python main.py localhost:8765 &"
+ssh cds-pi-3 "3.6venv; cd ~/de-r-pi-cluster; python main.py localhost:8765 &"
+
+fg
